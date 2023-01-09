743fb9d39cc3412fe37261b504182260d6bbe4f1
Abigael Mutheu <abigailmutheu3@gmail.com>
2023-01-09 11:50:26 -0800

    1-element_at.py edited

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,4 +5,4 @@ def element_at(my_list, idx):
     else:
         for i in range(0, len(my_list)):
             if i == idx:
-                return (my_list[idx]
+                return (my_list[idx])
